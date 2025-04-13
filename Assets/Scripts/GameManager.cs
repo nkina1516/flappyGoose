04b70d3c22dc8505ab4297d7ed13310aa227b4c2
Aly Abou-Zaid <aabouza@siue.edu>
2025-04-13 16:32:46 -0500

    Added GameManager script and new sprites (geese and trees)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private int score;
+
+    private void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+
+    private void IncreaseScore()
+    {
+        score++;
+    }
+
+}
